2a7e003cad8bce4970727a032ce8ef777564da8a
CihatGUVEN <89444171+CihatGUVEN@users.noreply.github.com>
2022-04-25 17:16:26 +0300

    spring-hello

---
+++ src/test/java/com/cihatguven/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cihatguven.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
